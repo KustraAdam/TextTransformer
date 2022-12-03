6b875dfcd9ab4f9b64458f7c932a06c3508a7f54
Zbigniew Apanowicz <jedrzej.gorski@student.put.poznan.pl>
2022-12-03 23:50:44 +0100

    Structured the transform method and implemented upper, lower, capitalize and inverse

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,5 +1,10 @@
 package pl.put.poznan.transformer.logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.lang.Character.*;
+
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
@@ -12,7 +17,72 @@ public class TextTransformer {
     }
 
     public String transform(String text){
-        // of course, normally it would do something based on the transforms
-        return text.toUpperCase();
+        // Perform transformations until the entirety of transforms has been traversed
+        for (String transform: this.transforms) {
+            switch (transform) {
+                case "upper":
+                    text = text.toUpperCase();
+                    break;
+                case "lower":
+                    text = text.toLowerCase();
+                    break;
+                case "capitalize":
+                    String[] separatedText = text.split(" ");
+                    ArrayList<String> capitalizedWords = new ArrayList<>();
+                    // Iterate over the space-separated substrings of the input
+                    for (String word : separatedText) {
+                        // If the substring is 1 character long, call toUpperCase() on it and add it to the output
+                        if (word.length() == 1) {
+                            capitalizedWords.add(word.toUpperCase());
+                        }
+                        // Otherwise, capitalize the first character and concatenate the rest of the substring unchanged
+                        else {
+                            capitalizedWords.add(word.substring(0, 1).toUpperCase() + word.substring(1));
+                        }
+                    }
+                    // The final output is made up of the contents of capitalizedWords joined together with a space
+                    text = String.join(" ", capitalizedWords);
+                    break;
+                case "inverse":
+                    Boolean[] letterCase = new Boolean[text.length()];
+                    char[] textArray = text.toCharArray();
+                    char[] transformedArray = new char[text.length()];
+                    // Iterate over every character in the input and set the corresponding index in letterCase
+                    // depending on whether the character is an upper case letter or not
+                    for (int i = 0; i < textArray.length; i++) {
+                        letterCase[i] = isUpperCase(textArray[i]);
+                    }
+                    // Iterate over every character in the input *backwards* and add it to the output, while changing
+                    // the case according to the value at the analogous index of letterCase (j)
+                    for (int i = textArray.length - 1; i >= 0; i--) {
+                        int j = textArray.length - 1 - i;
+                        if (letterCase[j]) {
+                            transformedArray[j] = toUpperCase(textArray[i]);
+                        }
+                        else {
+                            transformedArray[j] = toLowerCase(textArray[i]);
+                        }
+                    }
+                    text = String.valueOf(transformedArray);
+                    break;
+                case "numerals":
+                    // TODO: Implement conversion of numbers to numerals
+                    break;
+                case "abbreviate":
+                    // TODO: Implement abbreviations to na przykład; między innymi; i tym podobne
+                    break;
+                case "stands-for":
+                    // TODO: Implement converting abbreviations to their full form equivalents (prof.; dr.; np.; itd.)
+                    break;
+                case "escape":
+                    // TODO: Implement escaping LaTeX's special characters ($, &)
+                    break;
+                case "trim":
+                    // TODO: Implement removal of subsequent repetitions
+                    break;
+            }
+
+        }
+        return text;
     }
 }
