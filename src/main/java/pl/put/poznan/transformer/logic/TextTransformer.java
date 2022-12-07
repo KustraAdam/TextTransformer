6da2e5e395f7e019c160b1ac844a302aadd92a84
kklalolina <sztolcmankarolina@gmail.com>
2022-12-07 19:47:32 +0100

    abbreviate

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -2,6 +2,7 @@ package pl.put.poznan.transformer.logic;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static java.lang.Character.*;
 
@@ -69,40 +70,68 @@ public class TextTransformer {
                     // TODO: Implement conversion of numbers to numerals
                     break;
                 case "abbreviate":
-                    // TODO: Implement abbreviations to na przykład; między innymi; i tym podobne
+                    List<String> words = new ArrayList<String>(Arrays.asList(text.split(" ")));
+
+                    // Iterate over every element in the list and abbreviate words
+                    for(int i = 0 ; i < words.size(); i++) {
+                        if(words.get(i).equalsIgnoreCase("profesor")){
+                            words.set(i,words.get(i).substring(0, 4)+".");
+                        }
+                        else if(words.get(i).equalsIgnoreCase("doktor")){
+                            words.set(i,words.get(i).replace(words.get(i).substring(1,5),"")+".");
+                        }
+                        else if(i < words.size()-1 && words.get(i).equalsIgnoreCase("na") && words.get(i+1).equalsIgnoreCase("przykład")){
+                            words.set(i,words.get(i).substring(0,1)+words.get(i+1).charAt(0)+".");
+                            words.remove(i+1);
+                        }
+                        else if(i < words.size()-2 && words.get(i).equalsIgnoreCase("i") && words.get(i+1).equalsIgnoreCase("tak") && words.get(i+2).equalsIgnoreCase("dalej")){
+                            words.set(i,words.get(i).substring(0,1)+words.get(i+1).charAt(0)+words.get(i+2).charAt(0)+".");
+                            //Remove next element twice (as we want to remove two elements)
+                            words.remove(i+1);
+                            words.remove(i+1);
+                        }
+                    }
+
+                    //Creating string buffer to convert list to string
+                    StringBuffer sb1 = new StringBuffer();
+                    for(int i = 0; i < words.size()-1; i++) {
+                        sb1.append(words.get(i)+" ");
+                    }
+                    sb1.append(words.get(words.size()-1));
+
+                    text = sb1.toString();
                     break;
                 case "stands-for":
-                    //Extract words from text
-                    String[] temp = text.split(" ");
+                    String[] temparray = text.split(" ");
 
                     // Iterate over every word in the array and replace the abbreviations with full text
-                    for(int i=0;i<temp.length;i++){
-                        if(temp[i].equalsIgnoreCase("prof.")){
-                            if(temp[i].equals("PROF.")) temp[i]="PROFESOR";
-                            else temp[i]=temp[i].charAt(0)+"rofesor";
+                    for(int i = 0 ; i < temparray.length ; i++){
+                        if(temparray[i].equalsIgnoreCase("prof.")){
+                            if(temparray[i].equals("PROF.")) temparray[i]="PROFESOR";
+                            else temparray[i]=temparray[i].charAt(0)+"rofesor";
                         }
-                        else if(temp[i].equalsIgnoreCase("dr.")){
-                            if(temp[i].equals("DR.")) temp[i]="DOKTOR";
-                            else temp[i]=temp[i].charAt(0)+"oktor";
+                        else if(temparray[i].equalsIgnoreCase("dr.")){
+                            if(temparray[i].equals("DR.")) temparray[i]="DOKTOR";
+                            else temparray[i]=temparray[i].charAt(0)+"oktor";
                         }
-                        else if(temp[i].equalsIgnoreCase("np.")){
-                            if(temp[i].equals("NP.")) temp[i]="NA PRZYKLAD";
-                            else temp[i]=temp[i].charAt(0)+"a przykład";
+                        else if(temparray[i].equalsIgnoreCase("np.")){
+                            if(temparray[i].equals("NP.")) temparray[i]="NA PRZYKLAD";
+                            else temparray[i]=temparray[i].charAt(0)+"a przykład";
                         }
-                        else if(temp[i].equalsIgnoreCase("itd.")){
-                            if(temp[i].equals("ITD.")) temp[i]="I TAK DALEJ";
-                            else temp[i]=temp[i].charAt(0)+" tak dalej";
+                        else if(temparray[i].equalsIgnoreCase("itd.")){
+                            if(temparray[i].equals("ITD.")) temparray[i]="I TAK DALEJ";
+                            else temparray[i]=temparray[i].charAt(0)+" tak dalej";
                         }
                     }
 
                     //Creating string buffer to convert array to string
-                    StringBuffer sb = new StringBuffer();
-                    for(int i = 0; i < temp.length-1; i++) {
-                        sb.append(temp[i]+" ");
+                    StringBuffer sb2 = new StringBuffer();
+                    for(int i = 0; i < temparray.length-1; i++) {
+                        sb2.append(temparray[i]+" ");
                     }
-                    sb.append(temp[temp.length-1]);
+                    sb2.append(temparray[temparray.length-1]);
 
-                    text = sb.toString();
+                    text = sb2.toString();
                     break;
                 case "escape":
                     //Creating string buffer to easily insert a new character
